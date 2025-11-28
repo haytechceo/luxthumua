cd53ff2daf053be30d9fa5c0905e5537d085e1d5
haytechceo <haytechceo@gmail.com>
2025-11-28 14:52:54 +0700

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,12 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo.png" />
+         <Script 
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KXQFPGPL');`
+          }}
+        />
       </head>
       <body className={`${roboto.variable} antialiased`}>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-9FY6RLJ9YB" strategy="afterInteractive" />
@@ -107,6 +113,7 @@ export default function RootLayout({
             gtag('config', 'G-9FY6RLJ9YB');
           `}
         </Script>
+        
         <Suspense fallback={<div>Loading...</div>}>
           <StructuredData data={getOrganizationSchema()} />
           <StructuredData data={getLocalBusinessSchema()} />
